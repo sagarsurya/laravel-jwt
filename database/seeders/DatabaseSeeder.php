8ca5a6189fd1a3e73d75deac55fd1a4ee4ba6aef
Sagar Suryawanshi <sagarsuryawanshi191@gmail.com>
2024-06-10 23:44:24 +0530

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Sagar Suryawanshi',
+            'email' => 'sagar@gmail.com'
+        ]);
+    }
+}
